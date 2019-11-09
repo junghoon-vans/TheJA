78e50a4cafa14f0a6ff7e516bd7a420c9d8918ae
Jeonghun-Ban <bjh5230@gmail.com>
2019-11-10 00:43:28 +0900

    Create activity_bus_info

---
+++ app/src/main/java/com/example/theja/BusInfoActivity.java
@@ -0,0 +1,12 @@
+package com.example.theja;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BusInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_info);
+    }
+}
--- app/src/main/java/com/example/theja/MainActivity.java
+++ app/src/main/java/com/example/theja/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.theja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 대중교통 추가 버튼
+        Button addBus = (Button) findViewById(R.id.addVehicle);
+        addBus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BusInfoActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }

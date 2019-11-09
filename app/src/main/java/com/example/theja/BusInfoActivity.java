6a20c02bc14cae43d9e24521d7cebdbe61afc275
Jeonghun-Ban <bjh5230@gmail.com>
2019-11-10 00:52:25 +0900

    Add listview adapter in businfo

--- app/src/main/java/com/example/theja/BusInfoActivity.java
+++ app/src/main/java/com/example/theja/BusInfoActivity.java
@@ -1,6 +1,11 @@
 package com.example.theja;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BusInfoActivity extends AppCompatActivity {
@@ -8,5 +13,18 @@ public class BusInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_info);
+
+        // search
+        search = (EditText)findViewById(R.id.search);
+        search_button = (Button)findViewById(R.id.search_button);
+
+        // ListView
+        final ArrayAdapter<String> stationAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stationList);
+        final ListView stationListView = (ListView)findViewById(R.id.station);
+        stationListView.setAdapter(stationAdapter);
+
+        final ArrayAdapter<String> vehicleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, vehicleList);
+        final ListView vehicleListView = (ListView)findViewById(R.id.route);
+        vehicleListView.setAdapter(vehicleAdapter);
     }
 }

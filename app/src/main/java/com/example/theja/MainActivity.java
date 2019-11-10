b3fc6dcde490b7a1c9dc71e1f621f51e70bfd410
Jeonghun-Ban <bjh5230@gmail.com>
2019-11-10 15:45:51 +0900

    Add Function: add_bus_list in activity_main

--- app/src/main/java/com/example/theja/MainActivity.java
+++ app/src/main/java/com/example/theja/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.theja;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.theja.busInfo.BusArrival;
 import com.example.theja.busInfo.BusInfoActivity;
+import com.example.theja.busInfo.BusRoute;
+
+import java.util.List;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ListViewAdapter listViewAdapter;
+    private Vector<ListViewItem> vehicleList; // 리스트뷰 리스트
+
+    private BusArrival busArrival = new BusArrival(); // 버스 도착 정보 파싱 클래스
+    private List<String> msg; // 메시지
+
+    String key = "b6uH6X9Fql01CqlgeuVFN%2F8uAMSf061dkr86yJPO6BYMgFHAMoi9ZgK30BGNdSYywuZLyOnwjL9%2FtvT9iapVWQ%3D%3D"; // busInfo api key
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 대중교통 리스트뷰
+        listView = (ListView) findViewById(R.id.listView);
+        listViewAdapter = new ListViewAdapter();
+        listView.setAdapter(listViewAdapter);
+        vehicleList = listViewAdapter.listViewItemList;
 
         // 대중교통 추가 버튼
         Button addBus = (Button) findViewById(R.id.addVehicle);
@@ -26,4 +48,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // 대중교통 검색 결과 리턴
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            BusRoute bus = (BusRoute) data.getSerializableExtra("bus");
+            addListViewItem(bus.getArsId(), bus.getStNm(), bus.getBusRouteNm());
+        }
+    }
+
+    // 리스트뷰 추가 메소드
+    public void addListViewItem(final int arsId, final String stNm, final String routeNm) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+
+                msg = busArrival.getBusArrival(arsId, routeNm, key);
+
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+
+                        listViewAdapter.addItem(routeNm, stNm, msg.get(0), msg.get(1));
+
+                    }
+                });
+
+            }
+        }).start();
+    }
+
 }
--- app/src/main/java/com/example/theja/busInfo/BusArrival.java
+++ app/src/main/java/com/example/theja/busInfo/BusArrival.java
@@ -1,4 +1,88 @@
 package com.example.theja.busInfo;
 
-public class BusArrival {
+import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusArrival { // 버스 도착 정보
+
+    public String msg1, msg2;
+
+    public List<String> getBusArrival(int arsId, String routeNm, String key){
+        List<String> busArrival = new ArrayList<>();
+
+        String queryUrl = "http://ws.bus.go.kr/api/rest/stationinfo/getStationByUid" +
+                "?serviceKey=" + key + "&arsId=" + arsId;
+        try{
+            URL url = new URL(queryUrl); //검색 url
+            InputStream input = url.openStream();
+
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(input, "UTF-8") ); //inputstream 으로부터 xml 입력받기
+
+            String tag, text;
+            boolean rtNm = false;
+            boolean arrmsg1 = false;
+            boolean arrmsg2 = false;
+
+            xpp.next();
+            int eventType = xpp.getEventType();
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+
+                switch ( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        tag = xpp.getName();
+
+                        if(tag.equals("itemList")){
+                        } else if(tag.equals("arrmsg1")){
+                            arrmsg1 = true;
+                        } else if(tag.equals("arrmsg2")){
+                            arrmsg2 = true;
+                        } else if(tag.equals("rtNm")){
+                            rtNm = true;
+                        }
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        text = xpp.getText();
+                        if (arrmsg1){
+                            msg1 = text;
+                            arrmsg1 = false;
+                        } else if (arrmsg2){
+                            msg2 = text;
+                            arrmsg2 = false;
+                        } else if (rtNm){
+                            if(text.equals(routeNm)){
+                                busArrival.add(msg1);
+                                busArrival.add(msg2);
+                                break;
+                            }
+                            rtNm = false;
+                        }
+                        break;
+                    case XmlPullParser.END_TAG:
+                        break;
+                }
+                eventType=xpp.next();
+
+            }
+
+        } catch (Exception e) {
+            Log.e("에러", e.toString());
+        }
+
+        return busArrival;
+    }
+
 }

5a7a6b80d2d45e27d13e674e810c3f71d8baa934
Jeonghun-Ban <bjh5230@gmail.com>
2020-01-18 00:00:00 +0900

    Use ToolBar

--- app/src/main/java/com/example/theja/main/MainActivity.java
+++ app/src/main/java/com/example/theja/main/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.theja.R;
 import com.example.theja.UserData;
@@ -54,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar) ;
 
         // 대기정보 조회
         final ImageView mask = (ImageView) findViewById(R.id.mask);

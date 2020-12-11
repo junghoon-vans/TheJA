47c6cbe450db7bfa803ffc081c19f4d0e376bdff
Jeonghun-Ban <junghoon.ban@gmail.com>
2020-12-11 10:26:10 +0000

    fix: change api_key path

--- lib/auth/keys.dart
+++ lib/auth/keys.dart
@@ -3,22 +3,33 @@ import 'dart:convert' show json;
 import 'package:flutter/services.dart' show rootBundle;
 
 class Secret {
-  final String apiKey;
-  Secret({this.apiKey = ""});
+  final String thejaApiKey;
+
+  Secret({this.thejaApiKey = ""});
+
   factory Secret.fromJson(Map<String, dynamic> jsonMap) {
-    return new Secret(apiKey: jsonMap["api_key"]);
+    return new Secret(thejaApiKey: jsonMap["theja_apiKey"]);
   }
 }
 
 class SecretLoader {
   final String secretPath;
-  
-  SecretLoader({this.secretPath});
-  Future<Secret> load() {
-    return rootBundle.loadStructuredData<Secret>(this.secretPath,
+
+  static Secret _secret;
+
+  const SecretLoader({
+    this.secretPath = 'assets/secrets.json',
+  });
+
+  Future<Secret> load() async {
+    if (_secret != null) {
+      return _secret;
+    }
+    _secret = await rootBundle.loadStructuredData<Secret>(secretPath,
         (jsonStr) async {
       final secret = Secret.fromJson(json.decode(jsonStr));
       return secret;
     });
+    return _secret;
   }
 }
